546c29c9c515b8275f3be71821a0f47724945c39
Reacher <macryze@icloud.com>
2025-09-10 15:03:42 +0800

    refactor(backend): remove unused routes and update project setup
    - Remove genres and customers routes
    - Add dotenv configuration
    - Update MongoDB connection and error handling
    - Modify app.listen to use environment variable
    - Update package.json and package-lock.json with dotenv dependency

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
+require('dotenv').config();
+
 const mongoose = require('mongoose');
-const genres = require('./routes/genres');
-const customers = require('./routes/customers');
 const express = require('express');
 const app = express();
 
-mongoose.connect('mongodb://localhost/vidly')
-  .then(() => console.log('Connected to MongoDB...'))
-  .catch(err => console.error('Could not connect to MongoDB...'));
+mongoose
+  .connect('mongodb://localhost/vidly')
+  .then(() => console.log('Connected To MongoDB...'))
+  .catch((err) => console.log('Could Not Connect To MongoDB...'));
 
 app.use(express.json());
 app.use('/api/genres', genres);
-app.use('/api/customers', customers);
+app.use('api/customers', customers);
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+app.listen(port, () => console.log(`Listening on port: ${port}`));
