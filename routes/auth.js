129cb0324666aac61c3c8b8bbe806af94fe27f24
Reacher <macryze@icloud.com>
2025-09-14 22:10:29 +0800

    feat(auth): implement login functionality and update user credentials
    - Add login route and functionality in auth.js
    - Update user credentials in users.http
    - Create auth.http file for login requests

---
+++ routes/auth.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const { User } = require('../models/user');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const z = require('zod');
+
+router.post('/', async (req, res) => {
+  const { success, data, error } = validate(req.body);
+
+  if (!success)
+    return res.status(400).send(
+      error.issues
+        .map((issue) => {
+          return issue.path + ': ' + issue.message;
+        })
+        .join(', ')
+    );
+
+  let user = await User.findOne({ email: data.email });
+
+  if (!user) return res.status(400).send('Invalid Email or Password');
+
+  const result = await bcrypt.compare(data.password, user.password);
+  res.send(true);
+  console.log(result);
+});
+
+const validate = (req) => {
+  const schema = z.object({
+    email: z.email().min(5).max(255),
+    password: z.string().min(5).max(1024),
+  });
+  return schema.safeParse(req);
+};
+
+module.exports = router;
