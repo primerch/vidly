608a3245eebfc672a536e5973336bd7e18b6cbc1
Reacher <macryze@icloud.com>
2025-09-14 20:31:31 +0800

    feat(index): add auth route
    - Add auth route to the application
    - Connect auth route to the main server file

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
-const users = require('./routes/users')
+const users = require('./routes/users');
+const auth = require('./routes/auth');
 
 mongoose
   .connect('mongodb://localhost/vidly')
@@ -20,6 +21,7 @@ app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
 app.use('/api/users', users);
+app.use('/api/auth', auth);
 
 const port = process.env.PORT || 3000;
 
