ed73be0b8fabb490684a6cd0af600ea3a9d58c5c
Reacher <macryze@icloud.com>
2025-09-16 14:23:21 +0800

    fix: types/v3-directive not published

--- index.js
+++ index.js
@@ -5,13 +5,8 @@ require('winston-mongodb');
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
-const genres = require('./routes/genres');
-const customers = require('./routes/customers');
-const movies = require('./routes/movies');
-const rentals = require('./routes/rentals');
-const users = require('./routes/users');
-const auth = require('./routes/auth');
-const error = require('./middleware/error');
+
+require('./startup/routes')(app);
 
 winston.add(new winston.transports.File({ filename: 'logfile.log' }));
 winston.add(
@@ -56,15 +51,6 @@ mongoose
   .connect('mongodb://localhost/vidly')
   .then(() => console.log('Connected To MongoDB...'))
   .catch((err) => console.log('Could Not Connect To MongoDB...'));
-
-app.use(express.json());
-app.use('/api/genres', genres);
-app.use('/api/customers', customers);
-app.use('/api/movies', movies);
-app.use('/api/rentals', rentals);
-app.use('/api/users', users);
-app.use('/api/auth', auth);
-app.use(error);
 
 const port = process.env.PORT || 3000;
 
